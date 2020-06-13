1b174df5b1d718f93e1f9e5987bb4ef92adac477
s338523764 <60934483+s338523764@users.noreply.github.com>
2020-06-12 20:32:18 -0400

    Insert drawScene1.js file
    I copied and pasted my drawScene1.js file from p5.js

---
+++ drawScene1.js
@@ -0,0 +1,9 @@
+function drawScene1() {
+  currentScene = 1;
+  background(40, 200, 200);
+  textSize(30);
+  text("Welcome to the Art Show!", 20, 150);
+  textSize(15);
+  text("Sydney Hazelton", 130, 220)
+  btn1.draw();
+}
