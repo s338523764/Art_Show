f00449500b3a4b1b1257d3a44f8d80e32d8d6b13
s338523764 <60934483+s338523764@users.noreply.github.com>
2020-06-12 20:41:43 -0400

    Insert drawScene6.js file
    Copied and pasted my drawScene6.js file from p5.js

---
+++ drawScene6.js
@@ -0,0 +1,9 @@
+function drawScene6() {
+  currentScene = 6;
+  background("pink");
+  textSize(30);
+  fill("black");
+  text("Thank you for Visiting!", 50, 150);
+  textSize(15);
+  text("Have a Nice Day", 130, 220)
+}
